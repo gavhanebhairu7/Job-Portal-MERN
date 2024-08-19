a0872e4b8ca1157455eb8217359b2689d8f5f6fc
Bhairu <bhairugavhane5@gmail.com>
2024-08-20 01:01:56 +0530

    .

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,6 +1,6 @@
 export const sendToken = (user, statusCode, req, res) => {
   const token = user.getJwtToken()
-  const options = { expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), httpOnly: true }
+  const options = { expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), httpOnly: true, secure: true, sameSite: "None" }
 
   return res.status(statusCode).cookie("token", token, options).json({
     success: true,
